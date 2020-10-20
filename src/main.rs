e062e75a78173e1c04e19b062dd16eb1ee6d8c1b
Riey <creeper844@gmail.com>
2020-10-20 16:22:18 +0900

    Impl random spawn food

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use bevy::math::const_vec2;
 use bevy::prelude::*;
 use bevy::sprite::collide_aabb::collide;
-use rand::{thread_rng, Rng};
+use grid::Grid;
+use rand::{seq::IteratorRandom, thread_rng, Rng};
 
 const BACK_COLOR: Color = Color::BLACK;
+const GRID_SIZE: (usize, usize) = (600, 600);
 
 fn main() {
     App::build()
         .add_default_plugins()
         .add_resource(ClearColor(BACK_COLOR))
-        .add_resource(SimulationState::prepare(2, 20))
-        .add_resource(TurnTimer(Timer::from_seconds(20.0, true)))
+        .add_resource(SimulationState::prepare(10, 10, 5))
+        .add_resource(TurnTimer(Timer::from_seconds(10.0, true)))
         .add_resource(TurnCount(0))
         .add_startup_system(setup.system())
-        .add_system(simulation.system())
+        .add_system(prepare_simulation.system())
         .add_system(eat_system.system())
         .add_system(movement_system.system())
         .add_system(turn_system.system())
@@ -56,25 +59,29 @@ enum SimulationState {
     Prepare {
         creature_count: usize,
         food_count: usize,
+        daily_food_count: usize,
     },
-    Running,
+    Running {
+        daily_food_count: usize,
+    },
 }
 
 impl SimulationState {
-    pub fn prepare(creature_count: usize, food_count: usize) -> Self {
+    pub fn prepare(creature_count: usize, food_count: usize, daily_food_count: usize) -> Self {
         SimulationState::Prepare {
             creature_count,
             food_count,
+            daily_food_count,
         }
     }
 
-    pub fn running() -> Self {
-        SimulationState::Running
+    pub fn running(daily_food_count: usize) -> Self {
+        SimulationState::Running { daily_food_count }
     }
 
     pub fn is_running(&self) -> bool {
         match self {
-            SimulationState::Running => true,
+            SimulationState::Running { .. } => true,
             _ => false,
         }
     }
@@ -86,13 +93,19 @@ struct TurnCount(usize);
 struct Creature {
     life: usize,
     velocity: Vec2,
+    move_timer: Timer,
 }
 
 impl Creature {
+    pub const INIT_X: usize = 40;
+    pub const INIT_Y: usize = 40;
+    pub const INIT_SIZE: Vec2 = const_vec2!([Self::INIT_X as f32, Self::INIT_Y as f32]);
+
     pub fn new() -> Self {
         Self {
             life: 0,
             velocity: Vec2::new(0.0, 0.0),
+            move_timer: Timer::from_seconds(1.0, true),
         }
     }
 
@@ -111,64 +124,69 @@ impl Creature {
     pub fn velocity_mut(&mut self) -> &mut Vec2 {
         &mut self.velocity
     }
+
+    pub fn move_timer(&mut self) -> &mut Timer {
+        &mut self.move_timer
+    }
+
+    pub fn try_duplicate(&mut self) -> bool {
+        if self.life >= 2 {
+            self.life -= 1;
+            true
+        } else {
+            false
+        }
+    }
 }
 
 struct Food {}
 
 impl Food {
+    pub const INIT_X: usize = 20;
+    pub const INIT_Y: usize = 20;
+    pub const INIT_SIZE: Vec2 = const_vec2!([Self::INIT_X as f32, Self::INIT_Y as f32]);
+
     pub fn new() -> Self {
         Self {}
     }
 }
 
-fn simulation(
+fn prepare_simulation(
     mut commands: Commands,
     mut simulation: ResMut<SimulationState>,
     mut turn_count: ResMut<TurnCount>,
     sprites: Res<GameSprites>,
 ) {
-    match &mut *simulation {
-        SimulationState::Prepare {
-            creature_count,
-            food_count,
-        } => {
-            for i in 0..*creature_count {
-                commands
-                    .spawn(SpriteComponents {
-                        material: sprites.creature,
-                        transform: Transform::from_translation(Vec3::new(
-                            i as f32 * 60.0,
-                            0.0,
-                            1.0,
-                        )),
-                        sprite: Sprite::new(Vec2::new(40.0, 40.0)),
-                        ..Default::default()
-                    })
-                    .with(Creature::new());
-            }
-
-            for i in 0..*food_count {
-                commands
-                    .spawn(SpriteComponents {
-                        material: sprites.food,
-                        transform: Transform::from_translation(Vec3::new(
-                            i as f32 * 50.0,
-                            -100.0,
-                            1.0,
-                        )),
-                        sprite: Sprite::new(Vec2::new(20.0, 20.0)),
-                        ..Default::default()
-                    })
-                    .with(Food::new());
-            }
-
-            *simulation = SimulationState::running();
-            turn_count.0 = 0;
+    if let SimulationState::Prepare {
+        creature_count,
+        food_count,
+        daily_food_count,
+    } = &mut *simulation
+    {
+        for i in 0..*creature_count {
+            commands
+                .spawn(SpriteComponents {
+                    material: sprites.creature,
+                    transform: Transform::from_translation(Vec3::new(i as f32 * 60.0, 0.0, 1.0)),
+                    sprite: Sprite::new(Creature::INIT_SIZE),
+                    ..Default::default()
+                })
+                .with(Creature::new());
         }
 
-        SimulationState::Running => {
-            return;
+        for i in 0..*food_count {
+            commands
+                .spawn(SpriteComponents {
+                    material: sprites.food,
+                    transform: Transform::from_translation(Vec3::new(i as f32 * 50.0, -100.0, 1.0)),
+                    sprite: Sprite::new(Food::INIT_SIZE),
+                    ..Default::default()
+                })
+                .with(Food::new());
         }
+
+        *simulation = SimulationState::running(*daily_food_count);
+        turn_count.0 = 0;
     }
 }
 
@@ -205,34 +223,101 @@ fn movement_system(time: Res<Time>, mut creature_query: Query<(&Creature, &mut T
     }
 }
 
+fn calculate_random_objects(
+    object_width: usize,
+    object_height: usize,
+    count: usize,
+    translations: impl Iterator<Item = Vec3>,
+) -> impl Iterator<Item = Transform> {
+    let mut rng = thread_rng();
+
+    let mut grid = Grid::new(GRID_SIZE.0 / object_width, GRID_SIZE.1 / object_height);
+
+    for translation in translations {
+        let x = (((GRID_SIZE.0 / 2) as f32 + translation.x()).max(0.0) as usize) / object_width;
+        let x = x.min(grid.cols() - 1);
+        let y = (((GRID_SIZE.1 / 2) as f32 + translation.y()).max(0.0) as usize) / object_height;
+        let y = y.min(grid.cols() - 1);
+
+        grid[y][x] = true;
+    }
+
+    grid.iter_mut()
+        .enumerate()
+        .filter_map(|(i, v)| if *v { None } else { Some(i) })
+        .choose_multiple(&mut rng, count)
+        .into_iter()
+        .map(move |idx| {
+            let y = idx / grid.cols();
+            let x = idx - y * grid.cols();
+            Transform::from_translation(Vec3::new(
+                (x * object_width) as f32,
+                (y * object_height) as f32,
+                0.0,
+            ))
+        })
+}
+
 fn turn_system(
     mut commands: Commands,
     time: Res<Time>,
     simulation: Res<SimulationState>,
+    sprites: Res<GameSprites>,
     mut turn_timer: ResMut<TurnTimer>,
     mut turn_count: ResMut<TurnCount>,
-    mut creature_query: Query<(Entity, &mut Creature)>,
+    mut creature_query: Query<(Entity, &mut Creature, &Transform)>,
+    mut food_query: Query<(&Food, &Transform)>,
 ) {
-    if !simulation.is_running() {
-        return;
-    }
+    if let SimulationState::Running { daily_food_count } = &*simulation {
+        turn_timer.0.tick(time.delta_seconds);
 
-    turn_timer.0.tick(time.delta_seconds);
-
-    if !turn_timer.0.finished {
-        return;
-    }
-
-    turn_count.0 += 1;
-
-    for (creature_entity, mut creature) in &mut creature_query.iter() {
-        if creature.will_die() {
-            commands.despawn(creature_entity);
-        } else {
-            // TODO: duplicate creature
+        if !turn_timer.0.finished {
+            return;
         }
 
-        creature.time_pass();
+        turn_count.0 += 1;
+
+        // Process creature
+        for (creature_entity, mut creature, transform) in &mut creature_query.iter() {
+            if creature.will_die() {
+                commands.despawn(creature_entity);
+            } else {
+                if creature.try_duplicate() {
+                    commands
+                        .spawn(SpriteComponents {
+                            material: sprites.creature,
+                            sprite: Sprite::new(Creature::INIT_SIZE),
+                            transform: transform.clone(),
+                            ..Default::default()
+                        })
+                        .with(Creature::new());
+                }
+            }
+
+            creature.time_pass();
+        }
+
+        // Spawn foods
+
+        let mut food_iter = food_query.iter();
+
+        for transform in calculate_random_objects(
+            Food::INIT_X,
+            Food::INIT_Y,
+            *daily_food_count,
+            food_iter
+                .iter()
+                .map(|(_food, transform)| transform.translation()),
+        ) {
+            commands
+                .spawn(SpriteComponents {
+                    material: sprites.food,
+                    sprite: Sprite::new(Food::INIT_SIZE),
+                    transform,
+                    ..Default::default()
+                })
+                .with(Food::new());
+        }
     }
 }
 
@@ -249,12 +334,18 @@ fn life_display_system(
     }
 }
 
-fn random_move_system(mut creature_query: Query<(&mut Creature,)>) {
+fn random_move_system(time: Res<Time>, mut creature_query: Query<(&mut Creature,)>) {
     let mut rng = thread_rng();
 
     for (mut creature,) in &mut creature_query.iter() {
-        let x = rng.gen_range(-10.0, 10.0);
-        let y = rng.gen_range(-50.0, 10.0);
+        creature.move_timer.tick(time.delta_seconds);
+
+        if !creature.move_timer.finished {
+            continue;
+        }
+
+        let x = rng.gen_range(-50.0, 50.0);
+        let y = rng.gen_range(-50.0, 50.0);
         *creature.velocity_mut().x_mut() = x;
         *creature.velocity_mut().y_mut() = y;
     }
